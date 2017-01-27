164a74e15e3be47af073c9db88c7425408f26350
6linye9 <6linye9@gmail.com>
2017-01-27 11:32:26 -0500

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,4 +1,4 @@
 swig -c++ -python number.i
 g++ -fPIC -c number.cpp
-g++ -fPIC -c number_wrap.cxx -I/home/ye/anaconda3/include/python3.5m
+g++ -fPIC -c number_wrap.cxx -I/home/ye/anaconda3/include/python3.5m #find the location of "Python.h" file
 g++ -shared number.o number_wrap.o -o _number.so
